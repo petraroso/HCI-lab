7335a1c37c8eca41415f477ede5496a8bb820fd6
petraroso <proso00@fesb.hr>
2020-10-28 22:56:22 +0100

    First commit

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import ContactBar from '../components/contactBar'
+import NavigationHeader from '../components/NavigationHeader'
+
+
+const IndexPage = () => (
+  <main>
+    <ContactBar />
+    <NavigationHeader activeTab = "Home" />
+  </main>
+)
+
+export default IndexPage
