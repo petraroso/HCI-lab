bfaa3423b864e176d6554d8d448dd0cfe962631b
petraroso <proso00@fesb.hr>
2020-10-28 23:20:37 +0100

    adding footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,15 @@ import React from "react"
 
 import ContactBar from '../components/contactBar'
 import NavigationHeader from '../components/NavigationHeader'
+import Footer from '../components/Footer'
 
 
 const IndexPage = () => (
   <main>
     <ContactBar />
     <NavigationHeader activeTab = "Home" />
+    <Footer />
+
   </main>
 )
 
